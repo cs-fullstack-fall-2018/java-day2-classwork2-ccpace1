38b197e018e381156dc5e265d6b3bd1d6db7c304
Christopher Pace <Christopher_pace@ymail.com>
2018-07-23 14:40:31 -0500

    Commit of Exercise13

---
+++ Exercise13.java
@@ -0,0 +1,18 @@
+public class Exercise13 {
+
+    public static void counting1() {
+        int number1 = 0;
+        while (number1 <= 5) {
+            System.out.println(number1);
+            number1++;
+        }
+    }
+    public static void main(String[] args) {
+        int counter1 = 0;
+                while (counter1<=3) {
+            counting1();
+            counter1 ++;
+                }
+    }
+
+}
